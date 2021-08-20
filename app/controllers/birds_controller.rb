3dfecc63581902fd55d181e4cebc60bc8bd7d2bb
Carl <carlbrainegebe@gmail.com>
2021-08-20 03:13:10 -0500

    commit tree 632e81f90ff4cd036668b5ede37726e14240a6ae

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,8 +10,15 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    #create a new record and save to bird variable. However, if the creation isnt up to code with the creation requirements
+    # laid out in the model, it would not be valid(the valid? method would return false)
+    bird = Bird.create!(bird_params)
+      render json: bird, status: :created
+    # rescue ActiveRecord::RecordInvalid => invalid 
+      # if creation of record wasnt valid, a hash containing errors is sent back to the client
+      # render json: { errors: invalid.record.errors }, status: 422
+      # .errors looks like this { "name": ["can't be blank"] }
+    
   end
 
   # GET /birds/:id
@@ -21,9 +29,9 @@ class BirdsController < ApplicationController
 
   # PATCH /birds/:id
   def update
-    bird = find_bird
-    bird.update(bird_params)
-    render json: bird
+      bird = find_bird
+      bird.update!(bird_params)
+      render json: bird
   end
 
   # DELETE /birds/:id
@@ -45,6 +53,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: 422
   end
 
 end
